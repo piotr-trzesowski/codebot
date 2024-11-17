3d947ec5d83248458bdbd888578a9b9e83004394
piotrtrzesowski <piotr.trzesowski@icloud.com>
2024-11-17 18:13:43 +0100

    Add list of supported languages to the home.html select box.

--- codebot/website/views.py
+++ codebot/website/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    lang_list = ['bash', 'batch', 'c', 'clike', 'cpp', 'csharp', 'css', 'csv', 'cypher', 'dart', 'django', 'docker',
+                 'git', 'go', 'go-module', 'gradle', 'groovy', 'hsts', 'html', 'http', 'java', 'javascript', 'json',
+                 'json5', 'markup-templating', 'mongodb', 'objectivec', 'perl', 'php', 'plsql', 'powerquery',
+                 'powershell', 'python', 'r', 'regex', 'roboconf', 'robotframework', 'ruby', 'rust', 'sas', 'sass',
+                 'scala', 'sparql', 'sql', 'swift', 'turtle', 'yaml']
+
+    return render(request, 'home.html', {'lang_list': lang_list})
